558d0db9fb6c34e49f0ba885c1ec091d15dbcf61
James <lecjlg@leeds.ac.uk>
2017-04-19 12:17:47 +0100

    final script uploaded to the mega for the exercise

---
+++ readbme280_arranserialexercise_logger/readbme280_arranserialexercise_logger.ino
@@ -0,0 +1,75 @@
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+String pressurenow;
+
+#define SEALEVELPRESSURE_HPA (1024)
+Adafruit_BME280 bme; // I2C
+int incomingByte = 0; 
+unsigned long delayTime;
+
+void setup() {
+
+    Serial1.println(F("BME280 test"));
+    Serial1.begin(9600);
+
+    while(Serial1.available()) {
+    pressurenow= Serial1.readString();// read the incoming data as string
+    Serial1.println(pressurenow);
+
+}
+
+  // send data only when you receive data:
+        if (Serial1.available() > 0) {
+                // read the incoming byte:
+                incomingByte = Serial.read();
+
+                // say what you got:
+                Serial1.print("I received: ");
+                Serial1.println(incomingByte, DEC);
+        }
+
+    bool status;
+    
+    // default settings
+    status = bme.begin();
+    if (!status) {
+        Serial1.println("Could not find a valid BME280 sensor, check wiring!");
+        while (1);
+    }
+    
+    Serial1.println("Temperature,Pressure,Altitude,Humidity");
+    delayTime = 500;
+
+      
+}
+
+
+void loop() { 
+    printValues();
+    delay(delayTime);
+}
+
+
+void printValues() {
+    Serial1.print(bme.readTemperature());
+    Serial1.print(",");
+
+    Serial1.print(bme.readPressure() / 100.0F);
+    Serial1.print(",");
+
+
+    Serial1.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial1.print(",");
+
+
+    Serial1.print(bme.readHumidity());
+    Serial1.print(",");
+
+    Serial1.println();
+}
